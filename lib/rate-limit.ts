f226350f5edfa2e61e48253fe382a0d73d5ab3e2
Keith Scheldt <keithscheldt@gmail.com>
2023-08-02 16:43:45 -0400

    reinstalled upstash/ratelimit

---
+++ lib/rate-limit.ts
@@ -0,0 +1,15 @@
+// rate limiter
+
+import { Ratelimit } from "@upstash/ratelimit"
+import { Redis } from '@upstash/redis';
+
+export async function rateLimit(identifier: string) {
+    const rateLimit = new Ratelimit({
+        redis: Redis.fromEnv(),
+        limiter: Ratelimit.slidingWindow(10, "10 s"), // 10 request in 10 seconds
+        analytics: true,
+        prefix: "@upstash/ratelimit"
+    });
+
+    return await rateLimit.limit(identifier);
+}
