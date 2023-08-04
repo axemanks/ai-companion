53243e49636bc1f658c832b909c3b75a0ccf6583
Keith Scheldt <keithscheldt@gmail.com>
2023-08-03 20:06:51 -0400

    passed isPro to sidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -33,7 +33,7 @@ export const Navbar = ({
     <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
         <div className="flex items-center">
             {/* <Menu className="block md:hidden "/> */}
-            <MobileSidebar />
+            <MobileSidebar isPro={isPro} />
         <Link href="/" >
             {/* use cn here to add poppins(font.className) after tailwinds classes */}
             <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary text-sky-500", font.className)}>companion.ai</h1>
